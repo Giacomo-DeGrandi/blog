255240e430091cb94f3f7a96e1773ecc10e377eb
omar-diane <omar.diane@laplateforme.io>
2022-02-03 22:12:52 +0100

    Commit page profil Problème avec la fonction getConn

--- blogO&G/profil.php
+++ blogO&G/profil.php
@@ -9,30 +9,50 @@ session_abort();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Porfil</title>
+    <title>Mon Profil</title>
     <link rel="stylesheet" href="public/blog.css">
 </head>
 <body>
 
 <?php
 
+//Connexion à la base de donnée
 require ('config/config.php');
-$bd = new myDb ($conn);
-$conn = getConn (){
-    $conn=$this->conn;
-		return $conn;
-}
+require_once('model.php');
+$bd = new myDb ($server, $username, $password, $database);
+$conn = getConn();
+
 if(isset($_SESSION['id'])){
     $conn = "SELECT * FROM utilisateurs WHERE id = ?";
     $req = mysqli_fetch_array($_SESSION['id']);
+
+    if(isset($_POST['newlogin']) AND !empty($_POST['newlogin']) AND $_POST['newlogin'] != $user['login']){
+        //Maintenant je fais une requête SQL.
+        //Important de préciser l'id sinon tout les logins de chaque utilisateur dans la base de donnée seront mis à jour.
+        $insertlogin = $conn->prepare("UPDATE utilisateurs SET login = ? WHERE id = ?");
+        $insertlogin = execute(array($newlogin, $_SESSION['id']));
+    }
+
+    if(isset($_POST['newpassword']) AND !empty($_POST['newpassword']) AND $_POST['newpassword']){
+        $insertpassword = $conn->prepare("UPDATE utilisateurs SET password = ? WHERE id = ?");
+        $insertpassword = execute(array($newpassword, $_SESSION['id']));
+    }
+
+    if(isset($_POST['newemail']) AND !empty($_POST{'newemail'} AND $_POST{'newemail'} != $user['email'])){
+        $insertemail = $conn->prepare("UPDATE utilisateurs SET email = ? WHERE id = ?");
+        $insertemail = execute(array($newemail, $_SESSION['email']));
+    }
 }
 ?>
 <main>
     <h1>Mon Profil</h1>
 <form action="" method="post">
-<input type="text" name="login" value="<?php echo ?>" placeholder="Login">
+    <label> Login :</label>
+<input type="text" name="login" value="<?php echo $user['login']; ?>" placeholder="Login">
+<label> Password :</label>
 <input type="password" name="password" value="" placeholder="Password">
-<input type="email" name="email" value="" placeholder="Email">
+<label> Email :</label>
+<input type="email" name="email" value="<?php echo $user['email']; ?>" placeholder="Email">
 <input type="submit" name="submit" value="Modifier">
 </form>
 </main>
